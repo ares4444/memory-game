ebb0b50bce8d8328ad1bb8d5c39c8440a42b8cd0
Moises De Leon <mdeleon5826@gmail.com>
2022-04-02 11:34:23 -0400

    finished step-2, cards will now flip

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
         <MemoryCard />
         <MemoryCard />
       </div>
-      
     </div>
   );
 }
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './MemoryCard.css'
+class MemoryCard extends React.Component {
+    constructor() {
+        super();
+        this.state= {isFlipped: false}
+    }
 
-export default function MemoryCard() {
-  return (
-        <div className="MemoryCard">
-           <img className="logo" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="digitalcrafts-logo" /> 
-        </div>  )
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        });
+    }
+
+    render() {
+        let memoryCardInnerClass= "MemoryCardInner"
+
+        if (this.state.isFlipped) {
+            memoryCardInnerClass += " flipped";
+          }
+
+       return (
+            <>
+                <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                    <div className={memoryCardInnerClass}>
+                        <div className="MemoryCardBack">
+                            <img className="logo" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="digitalcrafts-logo" />
+                        </div> 
+                        <div className="MemoryCardFront">▲</div>
+                    </div>
+                </div>
+            </>  
+        ) 
+    }
 }
+
+export default MemoryCard;
+
+
